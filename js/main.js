2440b5f97a561938c6731582a917c0aeddac0770
Фарух Илиев <Mookie@server.jurinfo.zakon.kz>
2022-03-11 18:51:22 +0600

    more details 1.2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-const NAME_USERS = [
+const NAME_USERS = ([
   'Арнольд',
   'Сильвестр',
   'Чак',
@@ -7,27 +6,24 @@ const NAME_USERS = [
   'Вандам',
   'Лунгрен',
   'Ли',
-];
+]);
 
-// eslint-disable-next-line no-unused-vars
-const COMMENT_USERS = [
+const COMMENT_USERS = ([
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
+]);
 
 
-// eslint-disable-next-line no-unused-vars
-const DESCRIPTION_PHOTO = 25;
+const DESCRIPTION_PHOTO = (25);
 
 const stringLength = (string, maxlength) => string.length <= maxlength;
 stringLength('комментарий', 140);
 
-// eslint-disable-next-line no-unused-vars
-const getRandomNumber = (min, max) => {
+const getRandomNumber = ((min, max) => {
   if (min < 0 || max < 0) {
     return 0;
   }
@@ -43,35 +39,28 @@ const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+});
 
 const getUniqNumber = (maxMove) => {
   let currentNumber = 0;
-
-  const getNextNumber = () => {
-    if (maxMove === 1) {
-      return ++currentNumber;
-    }
+  if (maxMove === 1) {
     currentNumber += getRandomNumber(1, maxMove);
-    return currentNumber;
-  };
-  return getNextNumber;
+  }
+  return ++currentNumber;
 };
 
-const commentId = getUniqNumber(30);
+const commentId = () => getUniqNumber(30);
 
 const getComments = () => ({
   id: commentId(),
-  // eslint-disable-next-line no-template-curly-in-string
-  avatar: 'img/avatar-${getRandomNumber(1, 6)}.svg',
+  avatar: 'img/avatar{getRandomNumber(1, 6)}.svg',
   message: COMMENT_USERS[getRandomNumber(0, COMMENT_USERS.length - 1)],
   name: NAME_USERS[getRandomNumber(0, NAME_USERS.length - 1)]
 });
 
 const getDescriptionPhoto = (id) => ({
   id,
-  // eslint-disable-next-line no-template-curly-in-string
-  url: 'photos/${id}.jpg',
+  url: 'photos/{id}.jpg',
   description: 'Отлично покатались на лыжах',
   likes: getRandomNumber(10, 150),
   comments: Array.from({length: getRandomNumber(1, 2)}, getComments),
